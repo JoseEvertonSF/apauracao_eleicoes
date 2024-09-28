00dfc5a78e61fc5725d6b3e7fadab8960be0323b
JoseEvertonSF <joseeverton2@icloud.com>
2024-09-27 21:35:14 -0300

    Ajuste do schedule

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -12,7 +12,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule): void
     {
-        $schedule->command('apuracao-votos')->everyMinute();
+        $schedule->command('app:apuracao-votos')->everyMinute();
     }
 
     /**
